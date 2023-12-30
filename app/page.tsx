a676f6cdf4e86f2b1a5fb205209c8f0207b20fa4
brian <90752841+wokbjso@users.noreply.github.com>
2023-12-30 16:44:13 +0900

    Feat:Add svg 아이콘 컴포넌트로 사용할 수 있게 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import CI from "@common/assets/icons/food/restaurant.svg";
+
 export default function Home() {
-  return <main className="text-black body3-semibold">localmood</main>;
+  return (
+    <main className="text-black body3-semibold">
+      <CI />
+    </main>
+  );
 }
