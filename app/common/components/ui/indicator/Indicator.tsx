89f97c2e7345da9e36f45c5b3f9d40b7ac7c9cde
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-06 00:35:27 +0900

    Feat:생성 더미데이터 이용한 홈 화면 생성

--- app/common/components/ui/indicator/Indicator.tsx
+++ app/common/components/ui/indicator/Indicator.tsx
@@ -4,15 +4,27 @@ import IndicatorShape from "./IndicatorShape";
 interface IndicatorProps {
   index: number;
   className?: string;
+  handleIndicator: (index: number) => void;
 }
 
-export default function Indicator({ index, className }: IndicatorProps) {
+export default function Indicator({
+  index,
+  className,
+  handleIndicator,
+}: IndicatorProps) {
+  const indicatorChange = (index: number) => {
+    handleIndicator(index);
+  };
   return (
     <div className={twMerge("flex", className)}>
       {Array(4)
         .fill(null)
         .map((_, i) => (
-          <IndicatorShape key={i} indicate={index === i} />
+          <IndicatorShape
+            key={i}
+            indicate={index === i}
+            onClick={() => indicatorChange(i)}
+          />
         ))}
     </div>
   );
