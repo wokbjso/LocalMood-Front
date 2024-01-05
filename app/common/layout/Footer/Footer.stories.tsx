e89685d35d5020ff02903c25b954beb3833bc064
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-05 15:31:07 +0900

    Fix:수정 메인 페이지에 css 적용 안되는 현상 수정

--- app/common/layout/Footer/Footer.stories.tsx
+++ app/common/layout/Footer/Footer.stories.tsx
@@ -8,6 +8,9 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
+    nextjs: {
+      appDirectory: true,
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
