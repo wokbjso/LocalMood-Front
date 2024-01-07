534bc638778ac0b4177925068043f8c999fffe40
김지원 <jibom0507@naver.com>
2024-01-07 22:37:17 +0900

    Feat:큐레이션 Header 작성

--- app/feature/curation/components/CurationHeader/CurationHeader.tsx
+++ app/feature/curation/components/CurationHeader/CurationHeader.tsx
@@ -3,7 +3,9 @@
 export default function CurationHeader() {
   return (
     <div>
-      <div className="CurationHeader">큐레이션</div>
+      <div className="w-full h-[7.8rem] pt-[3.8rem] pr-[2rem] pb-[1.2rem] pl-[2rem]">
+        <p className="headline1 text-black">큐레이션</p>
+      </div>
     </div>
   );
 }
