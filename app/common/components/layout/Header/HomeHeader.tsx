d0c6de4f3abf4e0b107e7ac789f2968c014bb107
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-07 17:18:15 +0900

    Fix:수정 헤더에 버튼 나타날 시 헤더의 height가 미세하게 늘어나는 현상 수정

--- app/common/components/layout/Header/HomeHeader.tsx
+++ app/common/components/layout/Header/HomeHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import HeaderText from "@common/assets/icons/header/header.svg";
+import HeaderText from "@common/assets/icons/header/HeaderText";
 import SearchIcon from "@common/assets/icons/search/search.svg";
 import Button from "@common/components/ui/buttons/Button/Button";
 import useGetScrollHeight from "@common/hooks/useGetScrollHeight";
@@ -9,10 +9,12 @@ export default function HomeHeader() {
   const { scrollHeight } = useGetScrollHeight();
   return (
     <header className="fixed flex justify-between items-center px-[2rem] pt-[1.6rem] pb-[1rem] w-full z-10 bg-white">
-      <HeaderText />
+      <div className="py-[0.7rem]">
+        <HeaderText />
+      </div>
       <div className="flex items-center">
         {scrollHeight > 190 && (
-          <Button className="w-[12.5rem] h-[2.6rem] mr-[1.2rem] body2-semibold">
+          <Button className="w-[12.5rem] h-full py-[0.6rem] mr-[1.2rem] body2-semibold">
             키워드로 공간 찾기
           </Button>
         )}
