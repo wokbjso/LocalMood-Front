73fd35021b32d95ac51cb01401777f81370ccaac
김지원 <jibom0507@naver.com>
2024-01-10 02:22:44 +0900

    Feat:큐레이션 Tab에서 내 큐레이션과 스크랩 분리 및 스크랩 페이지 구현

--- app/(pages)/curation/page.tsx
+++ app/(pages)/curation/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import React, { useState } from "react";
 import CurationHeader from "@feature/curation/components/CurationHeader/CurationHeader";
 import Tab from "@common/components/ui/tab/Tab";
 import CurationMain from "@feature/curation/components/CurationMain/CurationMain";
 import CurationButton from "@feature/curation/components/CurationButton/CurationButton";
+import CurationScrapped from "@feature/curation/components/CurationScrapped/CurationScrapped";
 
 export default function Curation() {
   const CurationTabSections = [
@@ -13,6 +15,11 @@ export default function Curation() {
       text: "스크랩",
     },
   ];
+  const [tabIndex, setTabIndex] = React.useState(0);
+
+  const handleTabChange = (index: number) => {
+    setTabIndex(index);
+  };
 
   const curationMainPropsList = [
     {
@@ -73,25 +80,93 @@ export default function Curation() {
     },
   ]; //API 연결
 
+  const curationScrappedPropsList = [
+    {
+      id: 0,
+      curationPhoto: [
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      ],
+      userImg:
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      userName: "김현민",
+      mainText: "크리스마스에 즐기기 좋은 마포구 데이트 코스",
+      hashTags: ["연인과의 데이트", "크리스마스"],
+      scrapped: true,
+      onClick: () => {},
+    },
+    {
+      id: 1,
+      curationPhoto: [
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      ],
+      userImg:
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      userName: "김현민",
+      mainText: "크리스마스에 즐기기 좋은 마포구 데이트 코스",
+      hashTags: ["연인과의 데이트", "크리스마스"],
+      scrapped: true,
+      onClick: () => {},
+    },
+    {
+      id: 2,
+      curationPhoto: [
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      ],
+      userImg:
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      userName: "김현민",
+      mainText: "크리스마스에 즐기기 좋은 마포구 데이트 코스",
+      hashTags: ["연인과의 데이트", "크리스마스"],
+      scrapped: true,
+      onClick: () => {},
+    },
+    {
+      id: 3,
+      curationPhoto: [
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      ],
+      userImg:
+        "https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg",
+      userName: "김현민",
+      mainText: "크리스마스에 즐기기 좋은 마포구 데이트 코스",
+      hashTags: ["연인과의 데이트", "크리스마스"],
+      scrapped: true,
+      onClick: () => {},
+    },
+  ]; //API 연결
+
   return (
     <div className="Curation">
       <div>
         <CurationHeader />
-        <Tab sections={CurationTabSections} />
+        <Tab sections={CurationTabSections} onChange={handleTabChange} />
         <div className="px-[2rem] pb-[18.2rem] bg-background-gray-2 items-center">
-          <div className="flex items-center justify-between items-center pb-[0.6rem] pt-[2rem]">
-            <div className="flex body1 text-text-gray-8 items-center">
-              총{" "}
-              <p className="text-black">&nbsp;{curationMainPropsList.length}</p>{" "}
-              개
+          {tabIndex === 0 && (
+            <div className="flex items-center justify-between items-center pb-[0.6rem] pt-[2rem]">
+              <div className="flex body1 text-text-gray-8 items-center">
+                총{" "}
+                <p className="text-black">
+                  &nbsp;{curationMainPropsList.length}
+                </p>{" "}
+                개
+              </div>
+              <CurationButton />
             </div>
-            <CurationButton />
+          )}
+          {tabIndex === 0 &&
+            curationMainPropsList.map((props, index) => (
+              <div key={index} className="mb-[1.2rem]">
+                <CurationMain {...props} />
+              </div>
+            ))}
+          <div className="pt-[2rem]">
+            {tabIndex === 1 &&
+              curationScrappedPropsList.map((props, index) => (
+                <div key={index} className="mb-[1.6rem]">
+                  <CurationScrapped {...props} />
+                </div>
+              ))}
           </div>
-          {curationMainPropsList.map((props, index) => (
-            <div key={index} className="mb-[1.2rem]">
-              <CurationMain {...props} />
-            </div>
-          ))}
         </div>
       </div>
     </div>
