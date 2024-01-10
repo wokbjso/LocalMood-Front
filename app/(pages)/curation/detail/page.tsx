b7302bb0a156ae50ebfa0936632daf57588e0313
김지원 <jibom0507@naver.com>
2024-01-10 23:32:50 +0900

    Feat : 큐레이션 상세 페이지 생성

---
+++ app/(pages)/curation/detail/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function CurationDetail() {
+  return (
+    <div>
+      <div className="w-full h-[18.8rem] bg-background-secondary-normal">
+        <p className="headline1 text-black">큐레이션 상세</p>
+      </div>
+    </div>
+  );
+}
