2daa110656cb763d0bbf7b7ec40267bd0dc99c74
김지원 <jibom0507@naver.com>
2024-01-12 19:46:36 +0900

    Feat : 큐레이션 상세 페이지 CurationDetailTopBar 컴포넌트로 분리

--- app/(pages)/curation/detail/page.tsx
+++ app/(pages)/curation/detail/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import CurationTopAppBar from "@feature/curation/components/CurationTopAppBar/curationTopAppBar";
-import NonCuration from "@common/assets/icons/illustration/CurationNoSeat.png";
-import LockIcon from "@common/assets/icons/lock/lock.svg";
-import UnlockIcon from "@common/assets/icons/lock/unlock.svg";
 import ArrowRight from "@common/assets/icons/arrow/arror-right.svg";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import CurationDetailTopBar from "@feature/curation/components/CurationDetail/CurationDetailTopBar";
 
 export default function CurationDetail() {
   const router = useRouter();
@@ -15,35 +10,10 @@ export default function CurationDetail() {
   const navigateToHome = () => {
     router.push("/");
   };
-  const [isPublic, setIsPublic] = useState(true);
-
-  const togglePrivacy = () => {
-    setIsPublic((prevState) => !prevState);
-  };
 
   return (
     <div className="relative w-full">
-      <div className="h-[18.8rem] bg-background-secondary-normal">
-        <CurationTopAppBar />
-      </div>
-      <div className="w-full inline-flex flex-col -mt-[18.8rem] pt-[16.1rem] items-center pb-[2.4rem]">
-        <div className="w-[21.5rem] flex flex-col items-center">
-          <div className="w-[5.6rem] h-[5.6rem]">
-            <Image src={NonCuration} alt="NonCuration" />
-          </div>
-          <div className="max-w-[23.2rem] headline0 text-black text-center pt-[1.2rem] pb-[0.8rem]">
-            크리스마스에 즐기기 좋은 마포구 데이트 코스
-          </div>
-          <div
-            className="flex items-center gap-[0.4rem] body3-medium text-text-gray-8"
-            onClick={togglePrivacy}
-          >
-            {isPublic ? <LockIcon /> : <UnlockIcon />}
-            {isPublic ? "비공개" : "공개"}
-          </div>
-        </div>
-      </div>
-      <div className="w-full border-2 border-line-gray-3"></div>
+      <CurationDetailTopBar />
       <div className="w-full inline-flex flex-col items-center pt-[18.8rem] gap-[1.2rem]">
         <div className="headline1-semibold text-black">
           아직 저장된 공간이 없습니다.
---
+++ app/feature/curation/components/CurationDetail/CurationDetailTopBar.tsx
@@ -0,0 +1,39 @@
+import CurationTopAppBar from "@feature/curation/components/CurationTopAppBar/curationTopAppBar";
+import NonCuration from "@common/assets/icons/illustration/CurationNoSeat.png";
+import LockIcon from "@common/assets/icons/lock/lock.svg";
+import UnlockIcon from "@common/assets/icons/lock/unlock.svg";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function CurationDetailTopBar() {
+  const [isPublic, setIsPublic] = useState(true);
+
+  const togglePrivacy = () => {
+    setIsPublic((prevState) => !prevState);
+  };
+  return (
+    <div>
+      <div className="h-[18.8rem] bg-background-secondary-normal">
+        <CurationTopAppBar />
+      </div>
+      <div className="w-full inline-flex flex-col -mt-[18.8rem] pt-[16.1rem] items-center pb-[2.4rem]">
+        <div className="w-[21.5rem] flex flex-col items-center">
+          <div className="w-[5.6rem] h-[5.6rem]">
+            <Image src={NonCuration} alt="NonCuration" />
+          </div>
+          <div className="max-w-[23.2rem] headline0 text-black text-center pt-[1.2rem] pb-[0.8rem]">
+            크리스마스에 즐기기 좋은 마포구 데이트 코스
+          </div>
+          <div
+            className="flex items-center gap-[0.4rem] body3-medium text-text-gray-8"
+            onClick={togglePrivacy}
+          >
+            {isPublic ? <LockIcon /> : <UnlockIcon />}
+            {isPublic ? "비공개" : "공개"}
+          </div>
+        </div>
+      </div>
+      <div className="w-full border-2 border-line-gray-3"></div>
+    </div>
+  );
+}
