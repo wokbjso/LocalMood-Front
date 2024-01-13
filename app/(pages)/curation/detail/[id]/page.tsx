5479142c8b6e297959506528204e97fe6d59221c
김지원 <jibom0507@naver.com>
2024-01-13 12:44:00 +0900

    저장공간이 없을 때 띄우는 하단부 컴포넌트로 분리

--- app/(pages)/curation/detail/[id]/page.tsx
+++ app/(pages)/curation/detail/[id]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import ArrowRight from "@common/assets/icons/arrow/arror-right.svg";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import CurationDetailTopBar from "@feature/curation/components/CurationDetail/CurationDetailTopBar";
 import {
   curationMainPropsList,
   curationScrappedPropsList,
 } from "@feature/curation/dummyList";
+import CurationNoPlaces from "@feature/curation/components/CurationDetail/CurationNoPlaces";
 
 export default function CurationDetail(props: any) {
-  const router = useRouter();
   const pathname = usePathname();
   const parts = pathname.split(/[\/_]/);
   const id = parts[3];
@@ -26,10 +26,6 @@ export default function CurationDetail(props: any) {
     hashTags,
   } = selectedItem || {};
 
-  const navigateToHome = () => {
-    router.push("/");
-  };
-
   return (
     <div className="relative w-full">
       <CurationDetailTopBar
@@ -39,18 +35,8 @@ export default function CurationDetail(props: any) {
         places={places}
         hashTags={hashTags}
       />
-      <div className="w-full inline-flex flex-col items-center pt-[18.8rem] gap-[1.2rem]">
-        <div className="headline1-semibold text-black">
-          아직 저장된 공간이 없습니다.
-        </div>
-        <div
-          className="flex items-center gap-[0.4rem] body2-semibold text-text-gray-6"
-          onClick={navigateToHome}
-        >
-          공간 탐색하기
-          <ArrowRight />
-        </div>
-      </div>
+      {/*저장된 장소(places)가 0개일 때*/}
+      {places === 0 && <CurationNoPlaces />}
     </div>
   );
 }
---
+++ app/feature/curation/components/CurationDetail/CurationNoPlaces.tsx
@@ -0,0 +1,27 @@
+import ArrowRight from "@common/assets/icons/arrow/arror-right.svg";
+import { useRouter } from "next/navigation";
+
+export default function CurationNoPlaces() {
+  const router = useRouter();
+
+  const navigateToHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <div className="w-full inline-flex flex-col items-center pt-[18.8rem] gap-[1.2rem]">
+        <div className="headline1-semibold text-black">
+          아직 저장된 공간이 없습니다.
+        </div>
+        <div
+          className="flex items-center gap-[0.4rem] body2-semibold text-text-gray-6"
+          onClick={navigateToHome}
+        >
+          공간 탐색하기
+          <ArrowRight />
+        </div>
+      </div>
+    </div>
+  );
+}
