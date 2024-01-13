c033b76137b56be8ec8be5a8d416057ddb4055ee
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-13 17:31:43 +0900

    Design:홈 키워드 공간 찾기 버튼 너비 수정

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -145,8 +145,12 @@ export default function Home() {
         ))}
       </div>
       <div className="flex justify-center pb-[2rem] bg-[#F5F8FF]">
-        <LinkLayout routeUrl="/search" query={{ keyword_search: true }}>
-          <Button> 키워드로 공간 찾기</Button>
+        <LinkLayout
+          routeUrl="/search"
+          query={{ keyword_search: true }}
+          className="w-full flex justify-center px-[2rem]"
+        >
+          <Button>키워드로 공간 찾기</Button>
         </LinkLayout>
       </div>
       <div className="pb-[19.3rem] pt-[4rem]">
