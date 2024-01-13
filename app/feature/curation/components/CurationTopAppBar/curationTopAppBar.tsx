4d3b017ce6db69c6644a8cf872eb49bb95d6342c
김지원 <jibom0507@naver.com>
2024-01-14 03:23:15 +0900

    Feat : 큐레이션 상세 페이지 상단 바 BackIcon에 onclick 추가

--- app/feature/curation/components/CurationTopAppBar/curationTopAppBar.tsx
+++ app/feature/curation/components/CurationTopAppBar/curationTopAppBar.tsx
@@ -2,17 +2,23 @@ import BackIcon from "@common/assets/icons/arrow/arrow-left.svg";
 import ShareIcon from "@common/assets/icons/share/share.svg";
 import MenuIcon from "@common/assets/icons/menu/menu.svg";
 import ScrapLine from "@common/assets/icons/scrap/ScrapLine";
+import { useRouter } from "next/navigation";
 
 interface CurationTopAppBarProps {
   variant: string | undefined;
 }
 
 export default function CurationTopAppBar({ variant }: CurationTopAppBarProps) {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back(); // 이전 페이지로 이동
+  };
   return (
     <div className="pt-[5.3rem]">
       <div className="w-full h-[5.4rem] px-[2rem] py-[1.8rem]">
         <div className="flex items-center justify-between">
-          <div className="pr-[25.5rem]">
+          <div className="pr-[25.5rem]" onClick={handleBackClick}>
             <BackIcon />
           </div>
           <div className="flex gap-[0.8rem]">
