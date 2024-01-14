2c826c0b5c5d792ef563e2effafac07354c226a0
김지원 <jibom0507@naver.com>
2024-01-14 15:58:49 +0900

    Feat : 큐레이션 만들기 제목 세기 기능 추가

--- app/common/components/ui/textField/TextField.tsx
+++ app/common/components/ui/textField/TextField.tsx
@@ -1,12 +1,25 @@
+"use client";
+import React, { useState } from "react";
+
 export default function TextField() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
   return (
     <div className="pt-[4.2rem]">
       <div className="w-[33.5rem] pb-[1.2rem] flex justify-between items-center border-b border-text-gray-6">
         <input
-          className="w-[33.5rem] h-[2rem] headline1-semibold placeholder:headline1-semibold placeholder:text-text-gray-4"
+          className="w-[33.5rem] h-[2rem] headline1-semibold placeholder:headline1-semibold placeholder:text-text-gray-4 outline-none"
           placeholder="새 큐레이션"
+          value={inputValue}
+          onChange={handleInputChange}
         />
-        <div className="body2-medium text-text-gray-6">0/50</div>
+        <div className="body2-medium text-text-gray-6 pl-[1rem]">
+          {inputValue.length}/50
+        </div>
       </div>
     </div>
   );
