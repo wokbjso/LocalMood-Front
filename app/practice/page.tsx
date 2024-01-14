b026717687352f2d1ed44c7578b18a9b40108e4b
김지원 <jibom0507@naver.com>
2024-01-14 23:39:31 +0900

    Chore : 임시 페이지 삭제:

--- app/practice/page.tsx
+++
@@ -1,9 +0,0 @@
-import CurationMake from "@feature/curation/components/modal/CurationMake";
-
-export default function Practice() {
-  return (
-    <div>
-      <CurationMake />
-    </div>
-  );
-}
