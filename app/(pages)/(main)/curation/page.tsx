31985c0f0b8995477758457a91d7a7fd0bdaec85
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-15 18:41:58 +0900

    Refactor:큐레이션 만들기 모달 띄우는 로직 리팩토링

--- app/(pages)/(main)/curation/page.tsx
+++ app/(pages)/(main)/curation/page.tsx
@@ -10,9 +10,10 @@ import {
   curationScrappedPropsList,
 } from "@feature/curation/dummyList";
 import CurationMake from "@feature/curation/components/modal/CurationMake";
-import GrabModal from "@common/components/ui/modal/GrabModal";
+import UseCuration from "@feature/curation/useCuration";
 
 export default function CurationPage() {
+  const { tabIndex, isCurationMakeOpen, handlers } = UseCuration();
   const CurationTabSections = [
     {
       text: "내 큐레이션",
@@ -21,25 +22,11 @@ export default function CurationPage() {
       text: "스크랩",
     },
   ];
-  const [tabIndex, setTabIndex] = React.useState(0);
-  const [isCurationMakeOpen, setIsCurationMakeOpen] = useState(false);
-
-  const handleTabChange = (index: number) => {
-    setTabIndex(index);
-  };
-  const handleCurationButtonClick = () => {
-    setIsCurationMakeOpen(true);
-    document.body.style.overflow = "hidden";
-  };
-  const handleCloseModal = () => {
-    setIsCurationMakeOpen(false);
-    document.body.style.overflow = "unset";
-  };
 
   return (
     <div className="Curation h-[100vh] overflow-hidden">
       <CurationHeader />
-      <Tab sections={CurationTabSections} onChange={handleTabChange} />
+      <Tab sections={CurationTabSections} onChange={handlers.handleTabIndex} />
       <div className="h-full px-[2rem] pb-[18.2rem] bg-background-gray-2 items-center overflow-y-scroll">
         {tabIndex === 0 && (
           <div className="flex items-center justify-between pb-[0.6rem] pt-[2rem]">
@@ -48,7 +35,7 @@ export default function CurationPage() {
               <p className="text-black">&nbsp;{curationMainPropsList.length}</p>{" "}
               개
             </div>
-            <div onClick={handleCurationButtonClick}>
+            <div onClick={() => handlers.handleCurationMakeOpen(true)}>
               <CurationButton />
             </div>
           </div>
@@ -68,14 +55,10 @@ export default function CurationPage() {
             ))}
         </div>
       </div>
-      {isCurationMakeOpen && (
-        <>
-          <div className="absolute h-[100vh] w-full bg-black opacity-[0.4] z-10 bottom-0 left-0" />
-          <div className="fixed top-[4.6rem] bottom-0 left-0 right-0 z-10">
-            <CurationMake onClose={handleCloseModal} />
-          </div>
-        </>
-      )}
+      <CurationMake
+        isOpen={isCurationMakeOpen}
+        handleOpen={handlers.handleCurationMakeOpen}
+      />
     </div>
   );
 }
--- app/feature/curation/components/modal/CurationMake.tsx
+++ app/feature/curation/components/modal/CurationMake.tsx
@@ -5,29 +5,44 @@ import ButtonLock from "@common/components/ui/buttons/Button/ButtonLock";
 import CurationMakeKeyword from "./CurationMakeKeyword";
 
 interface CurationMakeProps {
-  onClose: () => void;
+  isOpen: boolean;
+  handleOpen: (state: boolean) => void;
 }
 
-export default function CurationMake({ onClose }: CurationMakeProps) {
+export default function CurationMake({
+  isOpen,
+  handleOpen,
+}: CurationMakeProps) {
+  const closeIconClicked = () => {
+    handleOpen(false);
+  };
   return (
-    <div>
-      <GrabModal />
-      <div className="bg-white">
-        <div className="h-[81.2rem] p-[2rem] flex flex-col items-center">
-          <div className="w-full grid justify-items-end">
-            <CloseIcon onClick={onClose} />
-          </div>
-          <div className="w-full">
-            <TextField />
-          </div>
-          <div className="w-full pt-[1.6rem] grid justify-items-end">
-            <ButtonLock />
-          </div>
-          <div className="w-full pt-[3.2rem] overflow-auto">
-            <CurationMakeKeyword />
+    isOpen && (
+      <>
+        <div className="absolute h-[100vh] w-full bg-black opacity-[0.4] z-10 bottom-0 left-0" />
+        <div className="fixed top-[4.6rem] bottom-0 left-0 right-0 z-10">
+          <GrabModal />
+          <div className="bg-white">
+            <div className="h-[81.2rem] p-[2rem] flex flex-col items-center">
+              <div
+                className="w-full grid justify-items-end"
+                onClick={closeIconClicked}
+              >
+                <CloseIcon />
+              </div>
+              <div className="w-full">
+                <TextField />
+              </div>
+              <div className="w-full pt-[1.6rem] grid justify-items-end">
+                <ButtonLock />
+              </div>
+              <div className="w-full pt-[3.2rem] overflow-auto">
+                <CurationMakeKeyword />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </>
+    )
   );
 }
