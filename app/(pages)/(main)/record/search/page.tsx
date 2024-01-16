08b7c5fd2de579997ae0bb232d5c1f12c6c39da5
김지원 <jibom0507@naver.com>
2024-01-16 22:40:32 +0900

    Feat : 공간 기록 페이지 TopBar 구현

---
+++ app/(pages)/(main)/record/search/page.tsx
@@ -0,0 +1,9 @@
+import PlaceRecordTopBar from "@feature/record/components/PlaceRecordTopBar/PlaceRecordTopBar";
+
+export default function RecordSearch() {
+  return (
+    <div>
+      <PlaceRecordTopBar showIndicator={false} text="스크랩한 공간" />
+    </div>
+  );
+}
---
+++ app/feature/record/components/PlaceRecordTopBar/PlaceRecordTopBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import ArrowIcon from "@common/assets/icons/arrow/arrow-left.svg";
+import Indicator from "@common/components/ui/indicator/Indicator";
+import { useRouter } from "next/navigation";
+
+interface RecordTopBarProps {
+  showIndicator: boolean;
+  text: string;
+}
+
+export default function PlaceRecordTopBar({
+  showIndicator,
+  text,
+}: RecordTopBarProps) {
+  const router = useRouter();
+  const handleBackClick = () => {
+    router.back(); // 이전 페이지로 이동
+  };
+
+  const handleIndicator = (index: number) => {
+    console.log(`Selected indicator index: ${index}`);
+  };
+
+  return (
+    <div className="flex flex-col items-start w-full px-[2rem] py-[1.2rem] gap-[1.2rem]">
+      <div onClick={handleBackClick}>
+        <ArrowIcon />
+      </div>
+      <div className="flex justify-between items-center self-stretch">
+        <div className="headline1-semibold text-black">{text}</div>
+        {showIndicator && (
+          <div className="flex justify-center items-center">
+            <Indicator
+              index={1}
+              handleIndicator={(index) => handleIndicator(index)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
