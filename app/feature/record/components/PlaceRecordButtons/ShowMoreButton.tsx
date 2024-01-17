7628ebc986be4e842cfb951e558a647d49adb977
김지원 <jibom0507@naver.com>
2024-01-18 00:09:49 +0900

    Feat : 더보기 버튼과 공간기록 검색 페이지 연결

--- app/feature/record/components/PlaceRecordButtons/ShowMoreButton.tsx
+++ app/feature/record/components/PlaceRecordButtons/ShowMoreButton.tsx
@@ -1,8 +1,18 @@
+"use client";
 import ArrowIcon from "@common/assets/icons/arrow/arrow-right.svg";
+import { useRouter } from "next/navigation";
 
 export default function ShowMoreButton() {
+  const router = useRouter();
+  const handleMoreClick = () => {
+    router.push("/record/search");
+  };
+
   return (
-    <div className="flex items-center gap-[1.2rem] text-text-gray-6 body2-semibold">
+    <div
+      className="flex items-center gap-[1.2rem] text-text-gray-6 body2-semibold"
+      onClick={handleMoreClick}
+    >
       <div> 더보기 </div>
       <div>
         <ArrowIcon />
