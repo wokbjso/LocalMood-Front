1849dd7eb5dadffee3c4e27939ce5f6062c1286f
김지원 <jibom0507@naver.com>
2024-01-21 02:14:17 +0900

    Feat : 검색바 뒤로가기 링크 추가

--- app/feature/record/components/PlaceSearch/PlaceSearchTopBar.tsx
+++ app/feature/record/components/PlaceSearch/PlaceSearchTopBar.tsx
@@ -1,10 +1,19 @@
+"use client";
 import ArrowIcon from "@common/assets/icons/arrow/arrow-left.svg";
 import PlaceSearchBar from "./PlaceSearchBar";
+import { useRouter } from "next/navigation";
 
 export default function PlaceSearchTopBar() {
+  const router = useRouter();
+  const handleBackClick = () => {
+    router.back(); //이전 페이지로 이동
+  };
+
   return (
     <div className="w-full flex px-[2rem] pt-[1.2rem] pb-[0.6rem] items-center gap-[1.2rem]">
-      <ArrowIcon />
+      <div onClick={handleBackClick}>
+        <ArrowIcon />
+      </div>
       <PlaceSearchBar />
     </div>
   );
