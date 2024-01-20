731940501bd049d500e73293f2cf63e9745015bd
김지원 <jibom0507@naver.com>
2024-01-21 02:32:21 +0900

    Feat : 검색 페이지와 키워드 선택 페이지 연결

--- app/(pages)/(main)/record/search/page.tsx
+++ app/(pages)/(main)/record/search/page.tsx
@@ -3,11 +3,19 @@ import PlaceInfoRecord from "@feature/place/components/PlaceInfoRecord/PlaceInfo
 import PlaceSearchTopBar from "@feature/record/components/PlaceSearch/PlaceSearchTopBar";
 import { RECORD_DUMMYLIST } from "@feature/record/dummyList";
 import PlaceRecordTopBar from "@feature/record/components/PlaceRecordTopBar/PlaceRecordTopBar";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function RecordSearch() {
   const searchParams = useSearchParams();
   const query = searchParams.get("key");
+  const router = useRouter();
+
+  const handlePlaceInfoRecordClick = (category: string, placeName: string) => {
+    router.push(
+      `/record/select/keyword?&category=${category}&name=${placeName}`
+    );
+  };
+
   return (
     <>
       {query === "select" && (
@@ -15,8 +23,13 @@ export default function RecordSearch() {
           <PlaceRecordTopBar showIndicator={false} text="스크랩한 공간" />
           <div className="inline-flex flex-col items-start w-full pt-[0.8rem] px-[2rem] gap-[0.8rem]">
             {RECORD_DUMMYLIST.map((props, index) => (
-              <div key={index} className="">
-                <PlaceInfoRecord {...props} />
+              <div key={index}>
+                <PlaceInfoRecord
+                  {...props}
+                  onClick={() =>
+                    handlePlaceInfoRecordClick(props.category, props.placeName)
+                  }
+                />
               </div>
             ))}
           </div>
@@ -28,8 +41,13 @@ export default function RecordSearch() {
           <PlaceSearchTopBar />
           <div className="inline-flex flex-col items-start gap-[0.8rem] px-[2rem] pt-[1.6rem]">
             {RECORD_DUMMYLIST.map((props, index) => (
-              <div key={index} className="">
-                <PlaceInfoRecord {...props} />
+              <div key={index}>
+                <PlaceInfoRecord
+                  {...props}
+                  onClick={() =>
+                    handlePlaceInfoRecordClick(props.category, props.placeName)
+                  }
+                />
               </div>
             ))}
           </div>
