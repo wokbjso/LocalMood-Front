9fd2769217d7cd5a38114161874a2b131c4fbca5
김지원 <jibom0507@naver.com>
2024-01-20 19:59:59 +0900

    Feat : 공간 기록 완료 페이지에서 '완료'누르면 공간기록 메인페이지로 이동

--- app/feature/record/components/BottomAppBar/BottomAppBar.tsx
+++ app/feature/record/components/BottomAppBar/BottomAppBar.tsx
@@ -1,5 +1,5 @@
 import Button from "@common/components/ui/buttons/Button/Button";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function BottomAppBar({
   hasFiltersSelected,
@@ -7,10 +7,18 @@ export default function BottomAppBar({
   hasFiltersSelected: boolean;
 }) {
   const pathname = usePathname();
+  const router = useRouter();
   const isComplete = pathname.endsWith("/complete");
+  const handleButtonClick = () => {
+    if (isComplete) {
+      router.push("/record");
+    } else {
+    }
+  };
+
   return (
     <div className="bg-white flex w-full h-[9.4rem] px-[2rem] pb-[4.6rem] flex-col items-center shrink-0">
-      <Button variant="fill" className="w-full">
+      <Button variant="fill" className="w-full" onClick={handleButtonClick}>
         {hasFiltersSelected ? "다음" : isComplete ? "결과 완료" : "건너뛰기"}
       </Button>
     </div>
