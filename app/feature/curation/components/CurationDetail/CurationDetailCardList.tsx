802af0d00c70cfda0e3ed04cff237fc75dd8ae28
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-22 01:44:16 +0900

    Feat:큐레이션 페이지에서 장소 버튼 클릭 시 해당 장소로 스크롤 기능 구현

--- app/feature/curation/components/CurationDetail/CurationDetailCardList.tsx
+++ app/feature/curation/components/CurationDetail/CurationDetailCardList.tsx
@@ -4,6 +4,7 @@ import LocationFillIcon from "@common/assets/icons/location/location-fill.svg";
 import Filter from "@common/components/ui/buttons/Filter/Filter";
 import CurationDetailInfoCard from "./CurationDetailInfoCard";
 import { PlaceInfoProps } from "@feature/place/type";
+import { createRef, useRef, useState } from "react";
 
 interface CurationDetailCardListProps {
   place_list: PlaceInfoProps[];
@@ -12,6 +13,14 @@ interface CurationDetailCardListProps {
 export default function CurationDetailCardList({
   place_list,
 }: CurationDetailCardListProps) {
+  const [placeIndex, setPlaceIndex] = useState(0);
+  const refs = useRef(
+    Array.from({ length: place_list.length }, () => createRef<HTMLDivElement>())
+  );
+  const handlePlaceFilterClick = (index: number) => {
+    setPlaceIndex(index);
+    refs.current[index].current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="pb-[6.1rem] p-[2rem] pr-0 w-full">
       <div className="w-full items-start">
@@ -21,22 +30,26 @@ export default function CurationDetailCardList({
             {place_list.length}개의 공간
           </p>
         </div>
-        <div className="flex gap-[0.8rem] mb-[2.4rem] overflow-x-scroll">
+        <div className="flex gap-[0.8rem] mb-[-10.6rem] overflow-x-scroll">
           {place_list.map((item, index) => (
             <Filter
               key={index}
               photo={item.placeImg[0]}
               label={item.placeName}
+              selected={placeIndex === index}
               className="whitespace-nowrap"
+              onClick={() => handlePlaceFilterClick(index)}
               // onClick 하면 get Data 변경
             />
           ))}
         </div>
       </div>
-      {place_list.map((props) => (
-        <div key={props.placeName}>
-          <CurationDetailInfoCard {...props} />
-        </div>
+      {place_list.map((props, i) => (
+        <CurationDetailInfoCard
+          key={props.placeName}
+          {...props}
+          ref={refs.current[i]}
+        />
       ))}
     </div>
   );
--- app/feature/curation/components/CurationDetail/CurationDetailInfoCard.tsx
+++ app/feature/curation/components/CurationDetail/CurationDetailInfoCard.tsx
@@ -1,69 +1,79 @@
 import ArrowIcon from "@common/assets/icons/arrow/arrow-right.svg";
-import Chip from "@common/components/ui/buttons/Chip/Chip";
 import ScrapFill from "@common/assets/icons/scrap/ScrapFill";
-import PlaceInfoMain from "@feature/place/components/PlaceInfoMain/PlaceInfoMain";
 import { PlaceInfoProps } from "@feature/place/type";
 import PlaceInfoBottom from "@feature/place/components/PlaceInfoBottom/PlaceInfoBottom";
 import Image from "next/image";
 import Slider from "@common/components/layout/Slider/Slider";
+import { ForwardedRef, forwardRef } from "react";
 
-export default function CurationDetailInfoCard({
-  id,
-  placeName,
-  placeImg,
-  category,
-  location,
-  scrapped,
-  tags,
-  tagsCategoryNum,
-  className,
-  imgClassName,
-  onClick,
-}: PlaceInfoProps) {
-  return (
-    <div className="w-full">
-      <Slider>
-        <div className="flex gap-[0.8rem]">
-          {placeImg?.map((url, i) => (
-            <div key={url + i} className="relative w-[28rem] h-[28rem]">
-              <Image
-                src={url}
-                alt="장소 사진"
-                fill
-                sizes="90vw"
-                className="rounded-[8px]"
-              />
-            </div>
-          ))}
-        </div>
-      </Slider>
-      <div>
-        <div className="w-full  pr-[1.9rem] pb-[4rem]">
-          <div className="pt-[2rem] pb-[1.6rem] flex justify-between ">
-            <div>
-              <div className="flex items-center gap-[0.4rem] text-black headline2-semibold">
-                {placeName}
-                <div className="px-[0.6rem] py-[0.4rem]">
-                  <ArrowIcon />
-                </div>
+const CurationDetailInfoCard = forwardRef(
+  (
+    {
+      id,
+      placeName,
+      placeImg,
+      category,
+      location,
+      scrapped,
+      tags,
+      tagsCategoryNum,
+      className,
+      imgClassName,
+      onClick,
+    }: PlaceInfoProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    return (
+      <div className="w-full pt-[13rem]" ref={ref}>
+        <Slider>
+          <div className="flex gap-[0.8rem]" ref={ref}>
+            {placeImg?.map((url, i) => (
+              <div key={url + i} className="relative w-[28rem] h-[28rem]">
+                <Image
+                  src={url}
+                  alt="장소 사진"
+                  fill
+                  sizes="90vw"
+                  className="rounded-[8px]"
+                />
               </div>
-              <div className="flex items-center gap-[0.8rem] pt-[0.8rem]">
-                <div className="body3-semibold text-text-gray-6">
-                  {category}
-                </div>
-                <div className="w-[0.1rem] h-[1.2rem] bg-text-gray-4"></div>
-                <div className="body3-medium text-text-gray-5">{location}</div>
-              </div>
-            </div>
-            <ScrapFill />
+            ))}
           </div>
-          <PlaceInfoBottom
-            tags={tags}
-            tagsCategoryNum={3}
-            bottomClassName="border-[0.1rem] border-line-gray-3 p-[1.6rem]"
-          />
+        </Slider>
+        <div className="mb-[-9rem]">
+          <div className="w-full pr-[1.9rem]">
+            <div className="pt-[2rem] pb-[1.6rem] flex justify-between ">
+              <div>
+                <div className="flex items-center gap-[0.4rem] text-black headline2-semibold">
+                  {placeName}
+                  <div className="px-[0.6rem] py-[0.4rem]">
+                    <ArrowIcon />
+                  </div>
+                </div>
+                <div className="flex items-center gap-[0.8rem] pt-[0.8rem]">
+                  <div className="body3-semibold text-text-gray-6">
+                    {category}
+                  </div>
+                  <div className="w-[0.1rem] h-[1.2rem] bg-text-gray-4"></div>
+                  <div className="body3-medium text-text-gray-5">
+                    {location}
+                  </div>
+                </div>
+              </div>
+              <ScrapFill />
+            </div>
+            <PlaceInfoBottom
+              tags={tags}
+              tagsCategoryNum={3}
+              bottomClassName="border-[0.1rem] border-line-gray-3 p-[1.6rem]"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+CurationDetailInfoCard.displayName = "CurationDetailInfoCard";
+
+export default CurationDetailInfoCard;
