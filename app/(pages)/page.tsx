fad59a62278ca39e660e5705d46e3c0d1cfdaae0
brian <90752841+wokbjso@users.noreply.github.com>
2024-01-22 15:15:55 +0900

    Refactor:홈 배너 컴포넌트로 분리

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,31 +1,14 @@
 import RightArrow from "@common/assets/icons/arrow/arrow-right.svg";
-import Chip from "@common/components/ui/buttons/Chip/Chip";
 import CurationHomeList from "@feature/curation/components/CurationHomeList/CurationHomeList";
 import HomeHeader from "@common/components/layout/Header/HomeHeader";
 import LinkLayout from "@common/components/layout/LinkLayout/LinkLayout";
-import Button from "@common/components/ui/buttons/Button/Button";
 import Slider from "@common/components/layout/Slider/Slider";
-import LampIcon from "@common/assets/icons/lamp/LampIcon";
 import Footer from "@common/components/layout/Footer/Footer";
-import WineIcon from "@common/assets/icons/wine/Wine";
-import ElectricCordIcon from "@common/assets/icons/electric-cord/ElectricCordIcon";
-import PetIcon from "@common/assets/icons/pet/PetIcon";
-import WindowIcon from "@common/assets/icons/window/WindowIcon";
-import HeartIcon from "@common/assets/icons/heart/HeartIcon";
-import MusicIcon from "@common/assets/icons/music/MusicIcon";
 import PlaceHomeSlider from "@feature/place/components/PlaceHomeSlider/PlaceHomeSlider";
 import PlaceInfoMain from "@feature/place/components/PlaceInfoMain/PlaceInfoMain";
+import HomeBanner from "./components/HomeBanner";
 
 export default function Home() {
-  const HEADER_KEYWORD = [
-    { icon: HeartIcon, color: "#F670C7", label: "연인과의 데이트" },
-    { icon: MusicIcon, color: "#8F73FD", label: "잔잔한 음악" },
-    { icon: LampIcon, label: "따뜻한 조명" },
-    { icon: WineIcon, label: "와인" },
-    { icon: ElectricCordIcon, label: "콘센트 있음" },
-    { icon: PetIcon, label: "애견동반 가능" },
-    { icon: WindowIcon, label: "통창뷰" },
-  ];
   //장소 get api 데이터로 대체(server side - fetch)
   const PLACE_DUMMY = [
     {
@@ -109,45 +92,7 @@ export default function Home() {
   return (
     <>
       <HomeHeader />
-      <p className="header-light pb-[2.4rem] px-[2rem] pt-[5.4rem] bg-background-secondary-light">
-        나에게 딱 맞는 공간을
-        <br />
-        <span className="header-main"> 키워드</span>로 찾아보세요
-      </p>
-      <div className="overflow-hidden">
-        <ul className="flex no-wrap">
-          <div className="header-slider1 flex pb-[2rem] bg-background-secondary-light">
-            {HEADER_KEYWORD.map((keyword, i) => (
-              <li key={keyword.label + i}>
-                <Chip className="whitespace-nowrap px-[1.12rem] flex items-center h-[3.2rem] mr-[1rem]">
-                  {<keyword.icon color={keyword.color} />}
-                  <span className="pl-[0.4rem]">{keyword.label}</span>
-                </Chip>
-              </li>
-            ))}
-          </div>
-          <div className="header-slider2 flex pb-[2rem] bg-background-secondary-light">
-            {HEADER_KEYWORD.map((keyword, i) => (
-              <li key={keyword.label + i}>
-                <Chip className="whitespace-nowrap px-[1.12rem] flex items-center h-[3.2rem] mr-[1rem]">
-                  {<keyword.icon color={keyword.color} />}
-                  <span className="pl-[0.4rem]">{keyword.label}</span>
-                </Chip>
-              </li>
-            ))}
-          </div>
-        </ul>
-      </div>
-      <div className="flex justify-center pb-[2rem] bg-background-secondary-light">
-        {/* search 하는 페이지로 이동 시 키워드 선택 모달 바로 뜨도록 쿼리로 상태 전달 */}
-        <LinkLayout
-          routeUrl="/search"
-          query={{ keyword_search: true }}
-          className="w-full flex justify-center px-[2rem]"
-        >
-          <Button>키워드로 공간 찾기</Button>
-        </LinkLayout>
-      </div>
+      <HomeBanner />
       <div className="pb-[12.3rem] pt-[4rem]">
         <PlaceHomeSlider mainText="연인과의 데이트" subText="를 위한 공간">
           <Slider className="mt-[1.6rem]">
