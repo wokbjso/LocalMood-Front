4d533b0b5de1e2898b21f2a1602702200cba50f2
김지원 <jibom0507@naver.com>
2024-01-29 17:07:12 +0900

    Feat : 큐레이션 상세 조회 페이지 api 연결

--- app/(pages)/(main)/curation/detail/[id]/page.tsx
+++ app/(pages)/(main)/curation/detail/[id]/page.tsx
@@ -14,84 +14,8 @@ export default async function CurationDetail({
 }: {
   params: { id: string };
 }) {
-  //api get 으로 가져오는 data로 대체
-
-  const CURATION_INFO = {
-    id: 0,
-    variant: "my",
-    user_name: "김현민",
-    curation_name: "크리스마스에 즐기기 좋은 마포구 데이트 코스",
-    hash_tags: ["연인과의 데이트", "크리스마스"],
-  };
-
-  const curationDetailCardList = [
-    {
-      id: 0,
-      placeName: "디벙크",
-      placeImg: [
-        "https://media.istockphoto.com/id/540233806/ko/%EC%82%AC%EC%A7%84/%EA%B5%AC%EC%9A%B4-%EC%87%A0%EA%B3%A0%EA%B8%B0-%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%81%AC.jpg?s=612x612&w=0&k=20&c=dDQxLsHVAzgJ1FQmiZMQeELPRyx9MlTyJKF7SwSV3n8=",
-        "https://media.istockphoto.com/id/1332101410/ko/%EC%82%AC%EC%A7%84/%EC%9C%A0%EB%A6%AC%EC%97%90-%EC%8F%9F%EC%95%84%EC%A7%80%EB%8A%94-%EB%A0%88%EB%93%9C-%EC%99%80%EC%9D%B8%EC%9D%98-%EC%9B%80%EC%A7%81%EC%9E%84%EC%9D%84-%EB%8F%99%EA%B2%B0%ED%95%A9%EB%8B%88%EB%8B%A4.jpg?s=612x612&w=0&k=20&c=moSOn9po9OAyJq30-_AVBCVCHxfPnUg6wliONyEAaGg=",
-        "https://media.istockphoto.com/id/1332101410/ko/%EC%82%AC%EC%A7%84/%EC%9C%A0%EB%A6%AC%EC%97%90-%EC%8F%9F%EC%95%84%EC%A7%80%EB%8A%94-%EB%A0%88%EB%93%9C-%EC%99%80%EC%9D%B8%EC%9D%98-%EC%9B%80%EC%A7%81%EC%9E%84%EC%9D%84-%EB%8F%99%EA%B2%B0%ED%95%A9%EB%8B%88%EB%8B%A4.jpg?s=612x612&w=0&k=20&c=moSOn9po9OAyJq30-_AVBCVCHxfPnUg6wliONyEAaGg=",
-      ],
-      category: "카페",
-      location: "마포구 망원동",
-      scrapped: false,
-      tags: {
-        purpose: ["연인과의 데이트", "작업/공부/책"],
-        interior: ["통창뷰", "넓은 공간"],
-        mood: ["모던한", "앤틱한"],
-      },
-    },
-    {
-      id: 1,
-      placeName: "나이스워크투데이",
-      placeImg: [
-        "https://media.istockphoto.com/id/1398630614/ko/%EC%82%AC%EC%A7%84/%EA%B5%AC%EC%9A%B4-%EB%A1%A4%EB%B9%B5%EC%97%90-%EB%B2%A0%EC%9D%B4%EC%BB%A8-%EC%B9%98%EC%A6%88%EB%B2%84%EA%B1%B0.jpg?s=612x612&w=0&k=20&c=VNy5uFGP5cL5cjnC9Z_kK5h8ciu4ckw45P-1NqBH9hg=",
-        "https://media.istockphoto.com/id/1197176660/ko/%EC%82%AC%EC%A7%84/%ED%99%88%EB%A9%94%EC%9D%B4%EB%93%9C-%ED%94%84%EB%A0%8C%EC%B9%98-%ED%94%84%EB%9D%BC%EC%9D%B4.jpg?s=612x612&w=0&k=20&c=_oD5ojTjO0ohZ1-z1s-kgcRvJrnOsgzQa5N-ZAoR7V8=",
-      ],
-      category: "카페",
-      location: "마포구 망원동",
-      scrapped: false,
-      tags: {
-        purpose: ["연인과의 데이트", "작업/공부/책"],
-        interior: ["통창뷰", "넓은 공간"],
-        mood: ["모던한", "앤틱한"],
-      },
-    },
-    {
-      id: 2,
-      placeName: "금복식당",
-      placeImg: [
-        "https://media.istockphoto.com/id/855749956/ko/%EC%82%AC%EC%A7%84/%EB%A0%88%EB%AA%AC-%EC%A1%B0%EA%B0%81%EC%9C%BC%EB%A1%9C-%EA%B5%AC%EC%9A%B4%EB%90%9C-%EB%8F%84%EB%AF%B8-%EC%83%9D%EC%84%A0.jpg?s=612x612&w=0&k=20&c=XSYfi8iwsIG3Z7H7ylRpQeJdeg5n4pD7kn08XxacMmc=",
-        "https://media.istockphoto.com/id/155372859/ko/%EC%82%AC%EC%A7%84/%EC%9D%B8%EB%AA%85%EB%B3%84-%EA%B3%A0%EA%B8%B0%EC%9E%A1%EC%9D%B4.jpg?s=612x612&w=0&k=20&c=oGwsokbGjGTTuBqkXYcAljfGUeKRed_VV_8QmMldlHQ=",
-      ],
-      category: "카페",
-      location: "마포구 망원동",
-      scrapped: false,
-      tags: {
-        purpose: ["연인과의 데이트", "작업/공부/책"],
-        interior: ["통창뷰", "넓은 공간"],
-        mood: ["모던한", "앤틱한"],
-      },
-    },
-    {
-      id: 3,
-      placeName: "맥도날드",
-      placeImg: [
-        "https://media.istockphoto.com/id/458737115/ko/%EC%82%AC%EC%A7%84/mcdonalds-%ED%8C%BB%EB%A7%90.jpg?s=612x612&w=0&k=20&c=nWHXncfC7EDWF8ZVWX8Hpvu5RkdrA5YV1P2MrkcKKCU=",
-        "https://media.istockphoto.com/id/1062831310/ko/%EC%82%AC%EC%A7%84/%EC%96%B4%EB%91%90%EC%9A%B4-%EB%B0%B0%EA%B2%BD%EC%97%90-%EC%96%BC%EC%9D%8C-%EC%8A%A4%ED%94%8C%EB%9E%98%EC%8B%9C%EC%99%80-%EC%9C%A0%EB%A6%AC%EC%97%90-%EC%B2%AD%EB%9F%89-%EC%9D%8C%EB%A3%8C%EB%A5%BC-%EB%B6%93%EB%8A%94-%EB%8B%A4.jpg?s=612x612&w=0&k=20&c=F2c6_btYvOwHtYR91AnH9XFfV3To9ZuKRXMyJiwQSRs=",
-      ],
-      category: "카페",
-      location: "마포구 망원동",
-      scrapped: false,
-      tags: {
-        purpose: ["연인과의 데이트", "작업/공부/책"],
-        interior: ["통창뷰", "넓은 공간"],
-        mood: ["모던한", "앤틱한"],
-      },
-    },
-  ];
   const curationDetail = await GetCurationDetail(id);
+  console.log(curationDetail);
 
   return (
     <div className="relative w-full pb-[18rem]">
--- app/feature/curation/queries/getCurationDetail.tsx
+++ app/feature/curation/queries/getCurationDetail.tsx
@@ -1,9 +1,12 @@
-export default async function GetCurationDetail(id: string) {
+export default async function GetCurationDetail(curationId: string) {
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJlbWFpbCI6InN0cmlkMjJnQG5hdmVyLmNvbSIsInJvbGUiOiLtmozsm5AiLCJpYXQiOjE3MDY0NTU4NzYsImV4cCI6MTcwNjQ5OTA3Nn0.o1x0nNZaQFZAHTziLTSLSjTrdMGJmv1SGp8EagLbe8NKSZ3HCI_lIUs-cQKZItmoZp4S6nFTpzFxRGWnBfiRHw";
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_API}/api/curation/detail/${id}`,
+    `${process.env.NEXT_PUBLIC_SERVER_API}/api/v1/curation/${curationId}`,
     {
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
     }
   );
