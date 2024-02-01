57f31befe03fac0c1e180401160994bdeaeb5756
brian <90752841+wokbjso@users.noreply.github.com>
2024-02-02 03:06:03 +0900

    Feat:공간 기록 검색에서 focus 시 바로 검색 창으로 이동되게 설정

--- app/feature/record/components/PlaceSearch/PlaceSearchBar.tsx
+++ app/feature/record/components/PlaceSearch/PlaceSearchBar.tsx
@@ -2,41 +2,42 @@
 
 import Search from "@common/assets/icons/search/search.svg";
 import Delete from "@common/assets/icons/close/close-gray.svg";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function PlaceSearchBar() {
   const router = useRouter();
-  const [searchText, setSearchText] = useState("");
-  const handleSearchTextChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newText = e.target.value;
-    setSearchText(newText);
-  };
-  const handleTextDelete = () => {
-    setSearchText("");
-  };
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleFocus = () => {
+      router.push("/record/search");
+    };
+
+    if (ref.current) {
+      ref.current.addEventListener("focus", handleFocus);
+    }
+
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener("focus", handleFocus);
+      }
+    };
+  }, [router]);
 
   return (
     <div className="flex w-full h-[4rem] pl-[1.2rem] pr-[0.6rem] py-[0.8rem] justify-between items-center relative self-stretch bg-background-gray-1 rounded-[1000px] gap-[0.8rem]">
       <div className="flex w-full items-center gap-[0.8rem]">
-        <Link
-          href={{
-            pathname: `record/search`,
-            query: { search_query: searchText },
-          }}
-        >
-          <Search />
-        </Link>
+        <Search />
         <input
+          ref={ref}
           className="body2-medium placeholder:text-text-gray-6 w-full bg-background-gray-1 outline-none"
-          value={searchText}
           placeholder="공간 이름을 검색해보세요"
-          onChange={handleSearchTextChange}
         />
       </div>
       <div>
-        <Delete onClick={handleTextDelete} />
+        <Delete />
       </div>
     </div>
   );
