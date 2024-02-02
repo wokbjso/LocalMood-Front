a8f76ce183583bd477c9d86a544fa36d0c7d91fa
Kim Hyeonmin <90752841+wokbjso@users.noreply.github.com>
2024-02-02 14:15:22 +0900

    Update page.tsx

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     "연인과의 데이트",
     "친구와의 만남",
     "왁자지껄 떠들 수 있는",
-    "대화에 집중할 수 있는",
+    "대화에 집중할 수 있는", 
   ];
   const randomPlace = await GetRandomPlaces();
   console.log(randomPlace);
