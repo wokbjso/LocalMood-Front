6bdf35607cd60e09204456bc234e2f07f3edbd89
brian <90752841+wokbjso@users.noreply.github.com>
2024-03-17 02:27:46 +0900

    Fix:GraphhUpDownVote className 설정 수정

--- app/common/components/ui/graph/GraphUpDownVote/GraphUpDownVote.tsx
+++ app/common/components/ui/graph/GraphUpDownVote/GraphUpDownVote.tsx
@@ -9,7 +9,7 @@ export default function GraphUpDownVote({
   percentage,
   like,
   className,
-}: GraphProps) {
+}: GraphProps & { className?: string }) {
   return (
     <div
       className={twMerge(
