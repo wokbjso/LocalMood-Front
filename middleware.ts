107ef4d350332365fe36b5627d976d6949ac235c
brian <90752841+wokbjso@users.noreply.github.com>
2024-04-10 16:06:20 +0900

    Fix:미들웨어 파일 콘솔 에러 수정

--- middleware.ts
+++ middleware.ts
@@ -2,35 +2,23 @@ import { getSession } from "@common/utils/session/getSession";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
-const host =
-  process?.env.NODE_ENV === "development"
-    ? "localhost:3000"
-    : "www.localmood.co.kr";
-
-const shouldLoginPath = [
-  `${protocol}://${host}/record`,
-  `${protocol}://${host}/record/:path*`,
-  `${protocol}://${host}/mypage`,
-  `${protocol}://${host}/curation`,
-  `${protocol}://${host}/curation/:path*`,
-];
-const shouldNotLoginPath = ["/login", "/login/:path*"];
+const shouldLoginPath = ["/record/:path*", "/mypage", "/curation/:path*"];
+const shouldNotLoginPath = ["/login/:path*"];
 
 export async function middleware(request: NextRequest) {
   const auth_info = await getSession();
   if (
     !auth_info?.data?.accessToken &&
-    shouldLoginPath.some((path) => request.url === path)
+    shouldLoginPath.some((path) => request.url.includes(path))
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   } else if (
     auth_info?.data?.accessToken &&
-    shouldNotLoginPath.some((path) => request.url === path)
+    shouldNotLoginPath.some((path) => request.url.includes(path))
   )
     return NextResponse.redirect(new URL("/", request.url));
 }
 
 export const config = {
-  matcher: [...shouldLoginPath, ...shouldNotLoginPath],
+  matcher: ["/record/:path*", "/mypage", "/curation/:path*", "/login/:path*"],
 };
