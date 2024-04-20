c6a7b01539cde00ea81e29a21cbbca7c9384e403
brian <90752841+wokbjso@users.noreply.github.com>
2024-04-20 13:55:52 +0900

    Fix:빌드 오류 해결

--- app/feature/search/queries/getTextSearchPlaceData.ts
+++ app/feature/search/queries/getTextSearchPlaceData.ts
@@ -3,12 +3,14 @@ import { SearchPlaceResponse } from "./dto/search-type";
 
 export async function getTextSearchPlaceData(
   name: string,
-  sortState: string
+  sortState?: string
 ): Promise<SearchPlaceResponse> {
   const auth_info = await getSession();
   const token = auth_info?.data?.accessToken;
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_API}/api/v1/spaces/search?sort=${sortState}`,
+    `${process.env.NEXT_PUBLIC_SERVER_API}/api/v1/spaces/search?sort=${
+      sortState ? sortState : "RECENT"
+    }`,
     {
       method: "POST",
       headers: {
