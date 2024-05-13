ebd4dcf2e8677973a28816e294676390a2942f8b
brian <90752841+wokbjso@users.noreply.github.com>
2024-05-14 00:47:01 +0900

    Fix:공간 상세 페이지 PlaceDetailInfoMore 컴포넌트 화살표 상호작용 범위 수정

--- app/feature/place/components/PlaceDetail/organisms/PlaceDetailInfoMore.tsx
+++ app/feature/place/components/PlaceDetail/organisms/PlaceDetailInfoMore.tsx
@@ -5,6 +5,7 @@ import { PlaceDetailInfoProps } from "@/feature/place/queries/dto/place-detail";
 import ArrowDownIcon from "@/common/assets/icons/arrow/ArrowDownIcon";
 import ArrowUpIcon from "@/common/assets/icons/arrow/ArrowUpIcon";
 import TextWithDivider from "@/common/components/ui/text/TextWithDivider";
+import { twMerge } from "tailwind-merge";
 
 //Organism
 export default function PlaceDetailInfoMore({
@@ -25,9 +26,9 @@ export default function PlaceDetailInfoMore({
   const formattedDishDesc = dishDesc?.split(",").join("・");
 
   return (
-    <div className="w-full flex items-center pt-[2.4rem]">
+    <div className="w-full flex items-center">
       <div className="w-full">
-        <div className="flex items-center  justify-between">
+        <div className="flex items-center justify-between">
           <div className="inline-flex items-center gap-[0.6rem]">
             {visitorNum && optionalService && (
               <>
@@ -40,19 +41,30 @@ export default function PlaceDetailInfoMore({
               </>
             )}
             {visitorNum && !optionalService && (
-              <span className="body2-medium text-text-gray-8">
+              <span
+                className={twMerge(
+                  "body2-medium text-text-gray-8 px-[2rem] pt-[24px]",
+                  !openMoreDetail ? "pb-[20px]" : ""
+                )}
+              >
                 {visitorNum}
               </span>
             )}
           </div>
           {formattedDishDesc !== "NULL" && (
-            <div onClick={moreButtonClicked}>
+            <div
+              onClick={moreButtonClicked}
+              className={twMerge(
+                "px-[2rem] pt-[24px]",
+                !openMoreDetail ? "pb-[20px]" : ""
+              )}
+            >
               {openMoreDetail ? <ArrowUpIcon /> : <ArrowDownIcon />}
             </div>
           )}
         </div>
         {openMoreDetail && formattedDishDesc !== null ? (
-          <div className="pt-[1.2rem]">
+          <div className="pt-[1.2rem] px-[20px] pb-[20px]">
             <span className="body2-medium">
               {type === "CAFE" ? "🍰" : "🍷"}
             </span>
