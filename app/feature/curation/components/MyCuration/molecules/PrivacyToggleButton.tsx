e18e4984e70d1a62e79ad0c007af9b4f0f81e8bb
brian <90752841+wokbjso@users.noreply.github.com>
2024-05-14 14:31:30 +0900

    Refactor:큐레이션 상세 페이지 공개/비공개 토글 로직 수정

--- app/feature/curation/components/MyCuration/molecules/PrivacyToggleButton.tsx
+++ app/feature/curation/components/MyCuration/molecules/PrivacyToggleButton.tsx
@@ -2,12 +2,11 @@
 
 import LockIcon from "@/common/assets/icons/lock/LockIcon";
 import UnlockIcon from "@/common/assets/icons/lock/UnlockIcon";
-import useFetching from "@/common/hooks/useFetching";
+import { queryFetchingSelector } from "@/common/state/queryFetching";
 import { toastInfoSelector } from "@/common/state/toast";
 import revalidateCurationDetail from "@/feature/curation/actions/revalidateCurationDetail";
 import revalidatePlaceDetail from "@/feature/place/actions/revalidatePlaceDetail";
 import { useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
 
 interface PrivacyToggleButtonProps {
@@ -24,8 +23,7 @@ export default function PrivacyToggleButton({
 
   const setToast = useSetRecoilState(toastInfoSelector);
 
-  const { isFetching, changeFetching } = useFetching();
-  const [show, setShow] = useState(privacy);
+  const setIsQueryFetching = useSetRecoilState(queryFetchingSelector);
 
   const togglePrivacy = async () => {
     const res = await fetch(`/api/curation/privacy/toggle/${id}`);
@@ -33,50 +31,29 @@ export default function PrivacyToggleButton({
     return res.status;
   };
 
-  const revalidateRelatedData = () => {
-    revalidateCurationDetail();
+  const revalidateRelatedData = async () => {
+    await revalidateCurationDetail().then(() => {
+      setIsQueryFetching(false);
+    });
     revalidatePlaceDetail();
     queryClient.invalidateQueries(["getMyCuration"]);
   };
 
   const handleClickTogglePrivacy = async () => {
-    setShow((prev) => !prev);
-    privacy
-      ? setToast({
-          open: true,
-          text: "비공개로 변경되었습니다",
-        })
-      : setToast({
-          open: true,
-          text: "공개로 변경되었습니다",
-        });
+    setIsQueryFetching(true);
+    if ((await togglePrivacy()) === 200) {
+      privacy
+        ? setToast({
+            open: true,
+            text: "비공개로 변경되었습니다",
+          })
+        : setToast({
+            open: true,
+            text: "공개로 변경되었습니다",
+          });
+      revalidateRelatedData();
+    }
   };
-
-  useEffect(() => {
-    const timer = setTimeout(async () => {
-      if (privacy === show) return;
-      if (isFetching) {
-        alert("이전 요청을 처리중입니다");
-        return;
-      } else {
-        changeFetching(true);
-        if ((await togglePrivacy()) === 200) {
-          changeFetching(false);
-          revalidateRelatedData();
-        }
-      }
-    }, 300);
-
-    return () => {
-      clearTimeout(timer);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [show]);
-
-  useEffect(() => {
-    if (privacy) setShow(true);
-    else setShow(false);
-  }, [privacy]);
 
   return (
     <>
@@ -84,8 +61,8 @@ export default function PrivacyToggleButton({
         className="flex items-center gap-[0.4rem] body3-medium text-text-gray-8"
         onClick={handleClickTogglePrivacy}
       >
-        {show ? <UnlockIcon /> : <LockIcon />}
-        {show ? "공개" : "비공개"}
+        {privacy ? <UnlockIcon /> : <LockIcon />}
+        {privacy ? "공개" : "비공개"}
       </div>
     </>
   );
