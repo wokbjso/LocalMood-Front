7390d3ed2c8d8661b5a171e4a74a4bce9a7c19a5
brian <90752841+wokbjso@users.noreply.github.com>
2024-05-19 18:39:02 +0900

    Feat:error.tsx 파일 생성

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
-import Text from "./common/components/ui/text/Text";
-import Title from "./common/components/ui/text/Title";
 
 export default function NotFound() {
   return (
     <div className="w-[100%] h-[100%] flex flex-col justify-center items-center">
-      <Title
-        title="요청하신 페이지를 찾을 수 없어요"
-        className="mb-[14px] headline1"
-      />
+      <p className="headline1 mb-[14px]">요청하신 페이지를 찾을 수 없어요</p>
       <Link href="/">
-        <Text text="로컬무드 홈으로 돌아가기" className="text-primary-normal" />
+        <p className="text-primary-normal headline2">
+          로컬무드 홈으로 돌아가기
+        </p>
       </Link>
     </div>
   );
