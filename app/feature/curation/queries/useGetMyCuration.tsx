d01aa9ace735bd37571ee08a1942d46f102c7dc4
brian <90752841+wokbjso@users.noreply.github.com>
2024-05-24 15:02:05 +0900

    Refactor:myCuration 데이터 staleTime,cacheTime Infinity로 설정

--- app/feature/curation/queries/useGetMyCuration.tsx
+++ app/feature/curation/queries/useGetMyCuration.tsx
@@ -26,6 +26,8 @@ export default function useGetMyCuration() {
     queryKey: ["getMyCuration"],
     queryFn: getMyCuration,
     retry: 0,
+    staleTime: Infinity,
+    cacheTime: Infinity,
     suspense: true,
   });
 }
