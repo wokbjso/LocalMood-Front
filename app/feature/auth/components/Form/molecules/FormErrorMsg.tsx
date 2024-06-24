d6e76d257de3cd46c5c384a483397877ab81cd35
brian <90752841+wokbjso@users.noreply.github.com>
2024-06-25 02:03:31 +0900

    test: 로컬 회원가입 관련 테스트코드 추가

--- app/feature/auth/components/Form/molecules/FormErrorMsg.tsx
+++ app/feature/auth/components/Form/molecules/FormErrorMsg.tsx
@@ -1,14 +1,15 @@
-import Text from "@/common/components/ui/text/Text";
-
 interface FormErrorMsgProps {
+  testId?: string;
   errorMsg?: string;
 }
 
 //Molecule
-export default function FormErrorMsg({ errorMsg }: FormErrorMsgProps) {
+export default function FormErrorMsg({ testId, errorMsg }: FormErrorMsgProps) {
   return (
     errorMsg && (
-      <Text className="body3-semibold text-error mt-[0.8rem]">{errorMsg}</Text>
+      <p data-testid={testId} className="body3-semibold text-error mt-[0.8rem]">
+        {errorMsg}
+      </p>
     )
   );
 }
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,1 @@
 import "@testing-library/jest-dom";
-import "./setupDomTests";
-global.TextEncoder = require("util").TextEncoder;
-global.TextDecoder = require("util").TextDecoder;
