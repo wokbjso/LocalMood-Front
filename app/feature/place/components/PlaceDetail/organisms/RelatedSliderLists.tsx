82ec19aa0abc1e173eef4573fc47c80f869d3ee0
brian <90752841+wokbjso@users.noreply.github.com>
2024-07-11 20:02:41 +0900

    refactor: 배포에서 streaming 안되는 현상 수정 위해 관련 정보 list에 ssr:false 적용

--- app/feature/place/components/PlaceDetail/organisms/RelatedSliderLists.tsx
+++ app/feature/place/components/PlaceDetail/organisms/RelatedSliderLists.tsx
@@ -1,23 +1,25 @@
-import GetPlaceRelatedInfo from "@/feature/place/queries/getPlaceRelatedInfo";
-import RelatedSlider from "./RelatedSlider";
+"use client";
+
 import PlaceInfoCard from "../../PlaceInfo/organisms/PlaceInfoCard";
 import CurationInfoCardDark from "@/feature/curation/components/CurationInfo/organisms/CurationInfoCardDark";
+import useGetPlaceRelatedInfo from "@/feature/place/queries/useGetPlaceRelatedInfo";
+import RelatedSlider from "./RelatedSlider";
 
 interface RelatedSliderListsProps {
   id: number;
   name: string;
 }
 
-export default async function RelatedSliderLists({
+export default function RelatedSliderLists({
   id,
   name,
 }: RelatedSliderListsProps) {
-  const relatedData = await GetPlaceRelatedInfo(id);
+  const { data: relatedData } = useGetPlaceRelatedInfo(id);
 
   return (
     <section className="pl-[2rem] w-[100%]">
       <RelatedSlider title={`${name}와(과) 비슷한 장소`}>
-        {relatedData.similarSpaceList.slice(0, 6).map((data) => (
+        {relatedData?.similarSpaceList.slice(0, 6).map((data) => (
           <PlaceInfoCard
             key={data.id}
             size="small"
@@ -26,9 +28,9 @@ export default async function RelatedSliderLists({
           />
         ))}
       </RelatedSlider>
-      {relatedData.relatedCurationList.length > 0 && (
+      {relatedData && relatedData.relatedCurationList.length > 0 && (
         <RelatedSlider title={`${name}이(가) 담긴 큐레이션`}>
-          {relatedData.relatedCurationList.map((data) => (
+          {relatedData?.relatedCurationList.map((data) => (
             <CurationInfoCardDark
               key={data.id}
               {...data}
